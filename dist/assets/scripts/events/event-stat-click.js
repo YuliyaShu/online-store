4b5c4798d60792f89ee1f1cff67ec67866dd20ce
YuliyaShu <juliette7373@gmail.com>
2022-09-20 10:08:59 +0200

    update

--- dist/assets/scripts/events/event-stat-click.js
+++
@@ -1,9 +0,0 @@
-import { createStatPage } from '../pages/page-stat';
-import { pageWrapper } from '../main-elements/body-wrapper';
-
-document
-  .querySelector('.header__stat')
-  .addEventListener('click', () => {
-    pageWrapper.element.children[1].innerHTML = '';
-    createStatPage();
-  });
--- dist/assets/scripts/main-elements/text-to-speech.js
+++
@@ -1,8 +0,0 @@
-function textToSpeech(text) {
-  const speech = new SpeechSynthesisUtterance(text);
-  speech.lang = 'en';
-  speech.text = text;
-  window.speechSynthesis.speak(speech);
-}
-
-export default textToSpeech;
